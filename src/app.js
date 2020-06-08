9a2e790850a037977bab148e9efda30465fe1f4b
Gloria <izayura@gmail.com>
2020-06-08 13:01:30 -0500

    User stories 1 and 2 DONE -Use of Fetch to manipulate API on the Project -Input and button were added to send MXN rate value -Fetch is inside function doingFetch() and function sendCurrency() is used to active it and show the currencies -<p> tags were added to show USD, EUR and CAD rate values in interface -USD, EUR and CAD currencies are show when button is clicked -Input to send MXN currency is set to an empty value when button is clicked

--- src/app.js
+++ src/app.js
@@ -1,1 +1,31 @@
-GET https://api.exchangeratesapi.io/latest HTTP/1.1
+doingFetch = () => {
+	fetch('https://api.exchangeratesapi.io/latest?base=MXN')
+		.then((response) => {
+			if (response.status !== 200) {
+				console.log('There was a problem. Status Code: ' + response.status);
+				return;
+			}
+			response.json().then((data) => {
+				console.log(data);
+				console.log(data.base);
+				var MxnRate = document.getElementById('MXN');
+				let MxnCurrency = MxnRate.value * data.rates.MXN;
+				console.log(MxnCurrency);
+				var UsdRate = document.getElementById('USD');
+				UsdRate.innerHTML = '$ ' + MxnCurrency * data.rates.USD;
+				var EurRate = document.getElementById('EUR');
+				EurRate.innerHTML = '€ ' + MxnCurrency * data.rates.EUR;
+				var CadRate = document.getElementById('CAD');
+				CadRate.innerHTML = '$ ' + MxnCurrency * data.rates.CAD;
+				MxnRate.value = '';
+			});
+		})
+		.catch((error) => {
+			console.log('Fetch Error :-S', error);
+		});
+};
+
+sendCurrency = () => {
+	doingFetch();
+	console.log('click');
+};
