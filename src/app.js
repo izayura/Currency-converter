31b9fe7c19192da0fdb56f65214de2c4e9b7ed92
Gloria <izayura@gmail.com>
2020-06-18 20:06:01 -0500

    New Class/Method create DOM -Class CreateRateContainer was created and extends from Rate -method createContainer() is used to create DOM elements with attributes from the object -changes in index.HTML to display DOM elements tests -changes in showRate() (DOESNT WORK CORRECTLY)

--- src/app.js
+++ src/app.js
@@ -5,6 +5,22 @@ class Rate{
 		this.symbol=symbol;
 	}
 }
+
+class CreateRateContainer extends Rate{
+	constructor(name, id){
+		super(name, id);
+	}
+
+		createContainer(){
+			var para = document.createElement("p");
+            var node = document.createTextNode(`${this.name}`);
+			para.appendChild(node);
+
+			var element = document.getElementById("containerRates");
+			var child = document.getElementById("testing");
+			element.insertBefore(para,child);
+		}
+}
 class DisplayRate extends Rate{
 	constructor(name, id, symbol){
 		super(name, id, symbol);
@@ -16,25 +32,40 @@ class DisplayRate extends Rate{
 		.then((data) => {
 			let MxnRate = document.getElementById('MXN');
 			let MxnCurrency =MxnRate.value * data.rates.MXN;
+			console.log(MxnCurrency);
 			let alert = document.getElementById('alert');
 			alert.innerHTML=MxnRate.validationMessage;
-			let getDOM = `document.getElementById("${this.id}")`
-			let convertingRate=`(${MxnCurrency}*data.rates.${this.name}).toFixed(2)`
+			let nameRate=this.name;
+			console.log(data);
+			console.log(nameRate);
+			let getDOM = `document.getElementById("${this.id}")`;
+			console.log(getDOM);
+			let getDataRate=`data.rates.${this.name}`;
+			//let myJSON = JSON.parse(getDataRate);
+			console.log(data.rates.CAD);
+			//console.log(myJSON);
+			console.log(getDataRate);
+			let result= "data.rates."+nameRate;
+			console.log(result);
+			let convertingRate=MxnCurrency*("data.rates."+nameRate);
+			console.log(convertingRate);
 			let message= `${this.symbol} `;
-			let displayRate=`${getDOM}.innerHTML=${message} ${convertingRate}`
+			let displayRate=`${getDOM}.innerHTML=${message} ${convertingRate}`;
 			console.log(displayRate);
 		    return displayRate;
-	})
+		})
+		.catch((error)=>{
+			console.log('Fetch Error :-S', error);
+		})
 	}
 }
 
+
 const UsdRate=new Rate("USD","USD","$");
-const EurRate=new Rate("EUR", "EUR", "€");
+const EurRate=new CreateRateContainer("EUR", "EUR", "€");
 const CadRate=new DisplayRate("CAD", "CAD", "$");
 
 console.log(UsdRate.name);
 console.log(EurRate.id);
 console.log(CadRate.symbol);
-//console.log(UsdRate.convertCurrency());
-//console.log(EurRate.showRate());
 console.log(CadRate.showRate());
