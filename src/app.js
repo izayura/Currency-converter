750575490cf92030597fe645cca2e92e737a7cbd
Gloria <izayura@gmail.com>
2020-06-19 12:15:28 -0500

    showRate() Progress -showRate() get API data correctly -showRate() display the converted rates in their respective sections(only works when page is loaded for the first time, CORRECT in next iteration) -"id" attribute is deleted(too repetitive, "name" works the same way) -button to test createContainer() functionality is deleted

--- src/app.js
+++ src/app.js
@@ -1,14 +1,13 @@
 class Rate{
-	constructor(name, id, symbol){
+	constructor(name, symbol){
 		this.name=name;
-		this.id=id;
 		this.symbol=symbol;
 	}
 }
 
 class CreateRateContainer extends Rate{
-	constructor(name, id, symbol){
-		super(name, id, symbol);
+	constructor(name, symbol){
+		super(name, symbol);
 	}
 
 		createContainer(){
@@ -16,7 +15,7 @@ class CreateRateContainer extends Rate{
 			currencyContainer.innerHTML+=
 			`
 			<section class="col-sm-12 col-md-3 py-2 my-2 bg-primary border heightCurrency">
-			<img src="img/${this.id}.png" alt="${this.name}">
+			<img src="img/${this.name}.png" alt="${this.name}">
             <h4>${this.name} Currency</h4>
             <p id="${this.name}" class="h3">${this.name}</p>
 			</section>
@@ -24,37 +23,26 @@ class CreateRateContainer extends Rate{
 		}
 }
 class DisplayRate extends CreateRateContainer{
-	constructor(name, id, symbol){
-		super(name, id, symbol);
+	constructor(name, symbol){
+		super(name, symbol);
 	}
 
 	showRate(){
 		return fetch('https://api.exchangeratesapi.io/latest?base=MXN')
 		.then(response=>response.json())
 		.then((data) => {
-			let MxnRate = document.getElementById('MXN');
+			let MxnRate = document                                              .getElementById('MXN');
 			let MxnCurrency =MxnRate.value * data.rates.MXN;
 			console.log(MxnCurrency);
 			let alert = document.getElementById('alert');
 			alert.innerHTML=MxnRate.validationMessage;
-			let nameRate=this.name;
-			console.log(data);
-			console.log(nameRate);
-			let getDOM = `document.getElementById("${this.id}")`;
+			let getDOM = document.getElementById(this.name);
 			console.log(getDOM);
-			let getDataRate=`data.rates.${this.name}`;
-			//let myJSON = JSON.parse(getDataRate);
-			console.log(data.rates.CAD);
-			//console.log(myJSON);
+			let getDataRate=data.rates[this.name];
 			console.log(getDataRate);
-			let result= "data.rates."+nameRate;
-			console.log(result);
-			let convertingRate=MxnCurrency*("data.rates."+nameRate);
+			let convertingRate=(MxnCurrency*getDataRate).toFixed(2);
 			console.log(convertingRate);
-			let message= `${this.symbol} `;
-			let displayRate=`${getDOM}.innerHTML=${message} ${convertingRate}`;
-			console.log(displayRate);
-		    return displayRate;
+			getDOM.innerHTML=`${this.symbol}  ${convertingRate}`;
 		})
 		.catch((error)=>{
 			console.log('Fetch Error :-S', error);
@@ -62,15 +50,16 @@ class DisplayRate extends CreateRateContainer{
 	}
 }
 
-
-const UsdRate=new DisplayRate("USD","USD","$");
-const EurRate=new DisplayRate("EUR", "EUR", "€");
-const CadRate=new DisplayRate("CAD", "CAD", "$");
+const UsdRate=new DisplayRate("USD","$");
+const EurRate=new DisplayRate("EUR", "€");
+const CadRate=new DisplayRate("CAD", "$");
 
 console.log(UsdRate.name);
-console.log(EurRate.id);
 console.log(CadRate.symbol);
+console.log(UsdRate.showRate());
 console.log(EurRate.showRate());
+console.log(CadRate.showRate());
+
 EurRate.createContainer();
 UsdRate.createContainer();
 CadRate.createContainer();
